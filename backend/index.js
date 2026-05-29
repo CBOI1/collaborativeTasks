e64fb60d8603d7f686e3f302b527a874160f7781
Christian Perez <christianperez9720@gmail.com>
2026-05-28 21:32:58 -0700

    Refactor routes into modules: user and tasks
    Index file containing routes was becoming too
    large with various routes consisting of different
    purposes. Grouped related routes into modules.

--- backend/index.js
+++ backend/index.js
@@ -4,16 +4,12 @@ const app = express();
 const session = require('express-session')
 const index = path.join(__dirname, "..", "frontend", "dist");
 require('dotenv').config({path: path.join(__dirname, ".env")});
-const validation = require(path.join(__dirname, "validation.js"));
-const db = require(path.join(__dirname, "database.js"));
-bcrypt = require("bcrypt");
-const {validationResult, matchedData} = require('express-validator');
 const PORT = 3000;
-const SALT_ROUNDS = 10;
-const httpCodes = require(path.join(__dirname, 'httpCodes.js'));
+const userRouter = require(path.join(__dirname, 'routes/user.js'));
+const taskRouter = require(path.join(__dirname, 'routes/task.js'));
 
 //serve react files 
-app.use('/', express.static(index));
+app.use(express.static(index));
 //parse json from request body
 app.use(express.json());
 app.use(session({
@@ -22,173 +18,7 @@ app.use(session({
     saveUninitialized: false,
 }));
 
-const parseIntBase10 = (num) => parseInt(num, 10);
-
-app.post('/api/register', 
-    validation.registerIsValid,
-    async (req, res) => {
-        //validate data:
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(httpCodes.BAD_REQUEST).json({
-                success: false,
-                message: "Validation Failed",
-                errors: errors.array()
-            })
-        }
-        //register user in the database
-        const data = matchedData(req);
-        const hash = await bcrypt.hash(data.password, SALT_ROUNDS);
-        const createdUser = await db.user.create({
-            data : {
-                email: data.email,
-                hash : hash
-            }
-        })
-        console.log(createdUser);
-        return res.json({
-            success : true
-        });
-    }
-);
-
-app.post('/api/login', 
-    validation.loginIsValid,
-    async (req, res) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(httpCodes.BAD_REQUEST).res.json({
-                user : null,
-                errors: errors.array()
-            });
-        } else {
-            const userInfo = matchedData(req);
-            const userObj = await db.user.findUnique({
-                where : {email : userInfo.email}
-            });
-            req.session.regenerate((err) => {
-                req.session.userId = userObj.id;
-                res.json({ user : userObj });
-            });
-        }
-    }
-);
-
-app.get('/api/me', async (req, res) => { 
-    if (req.
-        session.userId) {
-        const user = await db.user.findUnique({
-            where : { id : req.session.userId}
-        });
-        return res.json({user: user})
-    }
-     return res.status(httpCodes.BAD_REQUEST).json({user : null});
-});
-
-app.post('/api/logout', async (req, res) => {
-    if (req.session.userId) {
-        req.session.destroy();
-    }
-    return res.json({user : null});
-});
-
-function isAuthenticated(req, res, next) {
-    if (!req.session.userId) {
-        res.status(httpCodes.UNAUTHENTICATED).json(null);
-    } else {
-        next();
-    }
-}
-async function taskBelongsToUser(req, res, next) {
-    const userRecord = await db.task.findUnique({
-        where: { id: parseIntBase10(req.params.id) }
-    });
-    if (!userRecord || userRecord.userId != req.session.userId) {
-        return res.status(httpCodes.BAD_REQUEST).json(null);
-    }
-    next();
-}
-
-app.post('/api/tasks', isAuthenticated, async (req, res) => {
-    const result = await db.task.create({
-        data: {
-            title: req.body.title,
-            description: req.body.description,
-            userId: req.session.userId
-        }
-    });
-    res.json(result);
-});
-
-app.get('/api/tasks', isAuthenticated, async (req, res) => {
-    const userRecords = await db.task.findMany({
-        where: {
-            userId: req.session.userId
-        }
-    });
-    res.json(userRecords);
-});
-
-app.get('/api/tasks/:id', isAuthenticated, async (req, res) => {
-    const userRecord = await db.task.findUnique({
-        where: { id: parseIntBase10(req.params.id) }
-    });
-    if (!userRecord || userRecord.userId != req.session.userId) {
-        res.status(httpCodes.BAD_REQUEST).json(null);
-    } else {
-        res.json(userRecord);
-    }
-});
-
-
-app.post('/api/tasks/:id/update', isAuthenticated, taskBelongsToUser, validation.taskIsValid, async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(httpCodes.BAD_REQUEST).json({
-            errors: errors.array()
-        })
-    }
-    const updatedRecord = await db.task.update({
-        where : {
-            id : parseInt(req.params.id ?? null)
-        },
-        data : {
-            title : req.body.title,
-            description: req.body.description,
-            finished: req.body.finished,
-            updatedAt: new Date()
-        }
-    });
-    res.json(updatedRecord);
-});
-
-app.post('/api/tasks/create', isAuthenticated, validation.taskIsValid, async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(httpCodes.BAD_REQUEST).json({
-            errors: errors.array()
-        })
-    }
-    const createdRecord = await db.task.create({
-        data : {
-            title : req.body.title,
-            description: req.body.description,
-            finished: req.body.finished,
-            userId: req.session.userId
-        }
-    });
-    res.json(createdRecord);
-});
-
-app.delete("/api/tasks/:id/delete", isAuthenticated, taskBelongsToUser, async (req, res) => {
-    const deletedUser = await db.task.delete({
-        where: {
-            id: parseIntBase10(req.params.id)
-        }
-    });
-    res.json(deletedUser);
-});
-
-
+app.use('/api', userRouter);
+app.use('/api', taskRouter);
 
 app.listen(PORT);
