eb7fe4d3f52868e1d51f4c5878391e34ac92ecf1
Christian Perez <christianperez9720@gmail.com>
2026-06-09 15:50:58 -0700

    Refactor loaders
    Majority of the logic and structure was the same
    across route loaders. Implemented a general
    loader that accepts a closures to account
    for differences in path construction.

--- frontend/src/routes/loaders.js
+++ frontend/src/routes/loaders.js
@@ -1,5 +1,47 @@
-import { redirect } from "react-router-dom";
 
+const fetchResource = (urlConstructor, resourceName) => {
+   return async ({params}) => {
+    const resultObj = {}
+    const res = await fetch(urlConstructor(params), {credentials: "include"});
+    if (!res.ok) {
+      resultObj[resourceName] = null;
+    } else {
+      resultObj[resourceName] = await res.json();
+    }
+    return resultObj;
+ }
+}
+
+
+const loadUser = fetchResource((params) => '/api/me', 'user');
+const fetchTask = fetchResource((params) => `/api/projects/${params.pid}/tasks/${params.tid}`, 'task');
+const fetchProject = fetchResource((params) => `/api/projects/${params.pid}`, 'project');
+
+
+const fetchTasks = async ({params}) => {
+ if (params.pid === undefined) {
+   return {tasks : null};
+ }
+ const getTasks = fetchResource((params) => `/api/projects/${params.pid}/tasks`, 'tasks');
+ const resultObj = await getTasks({params});
+ return {pid : params.pid, ...resultObj};
+}
+
+
+
+
+
+
+export {
+ loadUser,
+ fetchTask,
+ fetchProject,
+ fetchTasks
+};
+
+
+
+/*
 const loadUser = async () => {
   const res = await fetch('/api/me', { credentials: 'include'});
   if (!res.ok) {
@@ -35,3 +77,4 @@ const fetchProject = async({params}) => {
 }
 
 export {loadUser, fetchTask, fetchTasks, fetchProject};
+*/
