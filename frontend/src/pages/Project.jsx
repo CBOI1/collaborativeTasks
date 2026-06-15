b335f2111c50b14e39464f98307841cdc52681c8
Christian Perez <christianperez9720@gmail.com>
2026-06-15 15:50:40 -0700

    Add character limit to project title

--- frontend/src/pages/Project.jsx
+++ frontend/src/pages/Project.jsx
@@ -1,14 +1,23 @@
 import styles from "./style.module.css"
-import { Form } from "react-router-dom";
-import { useRouteLoaderData } from "react-router-dom";
+import { Form, useRouteLoaderData } from "react-router-dom";
+import { useState } from "react"; 
+const TITLE_CHAR_LIMIT = 40;
 function Project() {
     const data = useRouteLoaderData("project-info");
-    return <div className="grow flex flex-col">
+    const [charCount, setCharCount] = useState(data ? data.project.title.length : 0);
+    return <div className="grow flex flex-col items-center">
         <Form method="POST" className="grow flex flex-col justify-center min-w-1/2 gap-4">
             <div className={styles.inputContainer}>
                 <label htmlFor="title">Title:</label>
-                <input type="text" id="title"  name="title" className={styles.inputStyle} defaultValue={data ? data.project.title : ""}/>
-                { /*<span>{`characters ${titleCharCount}/${TITLE_CHAR_LIMIT}`}</span> */ }
+                <input 
+                    type="text" 
+                    id="title"  
+                    name="title" 
+                    className={styles.inputStyle} 
+                    defaultValue={data ? data.project.title : ""} 
+                    onChange={e => setCharCount(e.currentTarget.value.length)}
+                />
+                { <span>{`characters ${charCount}/${TITLE_CHAR_LIMIT}`}</span> }
             </div>
             <button type="submit">{"Create"}</button>
         </Form>
